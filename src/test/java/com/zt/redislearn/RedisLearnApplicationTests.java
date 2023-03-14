6308dd02417b9eadcee682618b69d87691c0e070
zt <2916461184@qq.com>
2023-03-14 13:49:42 +0800

    开始学习redis

---
+++ src/test/java/com/zt/redislearn/RedisLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zt.redislearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
